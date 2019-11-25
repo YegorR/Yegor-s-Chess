021e860c0cd4f8b5ec2551fd676bb994eb40d44b
Egor Ryazantsev <egorryazancev1908@mail.ru>
2019-11-25 11:34:31 +0700

    HotSeatPlayer is added

---
+++ Assets/Scripts/ServerManager/HotSeatPlayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotSeatPlayer : IPlayer
+{
+    public event ActedHandler OnActEvent;
+
+    private ClientManager clientManager;
+    private PlayerColor playerColor;
+    private bool isActive = false;
+
+    public HotSeatPlayer(PlayerColor playerColor)
+    {
+        this.playerColor = playerColor;
+        clientManager = ClientManager.getInstance();
+        clientManager.ActEvent += OnPlayerAct;
+    }
+
+    public void SetGameSituation(GameSituation gameSituation)
+    {
+        if ((gameSituation.IsWhiteMoving && (playerColor == PlayerColor.White)) || 
+            (!gameSituation.IsWhiteMoving && (playerColor == PlayerColor.Black))) {
+            isActive = true;
+            clientManager.SetGameSituation(gameSituation);
+        }
+        else
+        {
+            isActive = false;
+        }
+        
+    }
+
+    private void OnPlayerAct(PlayerAct playerAct)
+    {
+        if (isActive)
+        {
+            OnActEvent(playerAct);
+        }
+    }
+}
