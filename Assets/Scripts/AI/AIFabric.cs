53a3f0da301d785e960cfdab707e042295b58ae3
Egor Ryazantsev <egorryazancev1908@mail.ru>
2019-12-04 20:53:57 +0700

    AIFabric is done

--- Assets/Scripts/AI/AIFabric.cs
+++ Assets/Scripts/AI/AIFabric.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class AIFabric
 {
+    public AIFabric()
+    {
+        PlayerColor playerColor = SceneData.PlayerColor;
+        HumanPlayer humanPlayer = new HumanPlayer(playerColor);
+        AIPlayer aiPlayer = new AIPlayer(playerColor == PlayerColor.White? PlayerColor.Black : PlayerColor.White);
 
+        IPlayer whitePlayer; IPlayer blackPlayer;
+        if(playerColor == PlayerColor.White)
+        {
+            whitePlayer = humanPlayer;
+            blackPlayer = aiPlayer;
+        }
+        else
+        {
+            whitePlayer = aiPlayer;
+            blackPlayer = humanPlayer;
+        }
+        ServerManager serverManager = new ServerManager(whitePlayer, blackPlayer);
+    }
 }
