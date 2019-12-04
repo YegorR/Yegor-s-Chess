fefd636bb3d7d2cad6d4356126b2011d8bae35d5
Egor Ryazantsev <egorryazancev1908@mail.ru>
2019-12-04 20:45:32 +0700

    HumanPlayer is added

---
+++ Assets/Scripts/ServerManager/HumanPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanPlayer : IPlayer
+{
+    private ClientManager clientManager;
+    private PlayerColor playerColor;
+
+    public event PlayerActedEventHandler PlayerActedEvent;
+
+    public HumanPlayer(PlayerColor playerColor)
+    {
+        this.playerColor = playerColor;
+        clientManager = ClientManager.getInstance();
+        clientManager.ActEvent += OnPlayerMoved;
+    }
+
+    public void SetGameSituation(GameSituation gameSituation)
+    {
+        clientManager.SetGameSituation(gameSituation);
+        if ((playerColor == PlayerColor.White) && (gameSituation.IsWhiteMoving))
+        {
+            clientManager.Block(false, PlayerColor.White);
+        }
+        else if ((playerColor == PlayerColor.Black) && (!gameSituation.IsWhiteMoving))
+        {
+            clientManager.Block(false, PlayerColor.Black);
+        }
+        else
+        {
+            clientManager.Block(true, PlayerColor.White);
+            clientManager.Block(true, PlayerColor.Black);
+        }
+    }
+
+    private void OnPlayerMoved(PlayerAct playerAct)
+    {
+        clientManager.Block(true, PlayerColor.White);
+        clientManager.Block(true, PlayerColor.Black);
+        PlayerActedEvent(playerAct);
+    }
+}
