aa02a82a4359a3fbb54dd26b2386b24b3ad80698
Egor Ryazantsev <egorryazancev1908@mail.ru>
2019-12-04 20:23:26 +0700

    AIPlayer is done

--- Assets/Scripts/AI/AIPlayer.cs
+++ Assets/Scripts/AI/AIPlayer.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class AIPlayer : IPlayer
 {
+    private const string RELATIVE_ENGINE_PATH = "/Engine/stockfish_10_x64.exe";
+    private readonly UCIAdapter uciAdapter;
+    private PlayerColor playerColor;
+
     public event PlayerActedEventHandler PlayerActedEvent;
 
+    public AIPlayer(PlayerColor playerColor)
+    {
+        this.playerColor = playerColor;
+        uciAdapter = new UCIAdapter(Application.dataPath + RELATIVE_ENGINE_PATH);
+        uciAdapter.EngineMovedEvent += OnEngineMoved;
+        uciAdapter.Start();
+    }
     public void SetGameSituation(GameSituation gameSituation)
     {
-        throw new System.NotImplementedException();
+        uciAdapter.SetGameSituation(gameSituation);
+    }
+
+    private void OnEngineMoved(Cell from, Cell to)
+    {
+        PlayerAct playerAct = new PlayerAct()
+        {
+            Act = PlayerAct.ActType.Move,
+            From = from,
+            To = to,
+            PlayerColor = playerColor
+        };
+        PlayerActedEvent(playerAct);
     }
 }
